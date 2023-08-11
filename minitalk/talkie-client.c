564f0292dc02c0a85667804893fc12b1b2327e15
lele <drieskensl@gmail.com>
2023-08-11 09:59:32 +0200

    finished i think

--- minitalk/talkie-client.c
+++ minitalk/talkie-client.c
@@ -12,89 +12,42 @@
 
 #include "minitalk.h"
 
-/*
- * Checklist client : 
- * 		Takes the PID of the server to send to and the message
- * 		Encrypt the message (bits)
- * 		Send the message
- * 		Create a stop condition so the server knows the message ended
-*/
-
-int	dec_to_bin(int dec)
-{
-	int	bin;
-	int	bit;
-
-	bin = 0;
-	bit = 1;
-	while (dec > 0)
-	{
-		if (dec % 2 == 1)
-			bin += bit;
-		dec /= 2;
-		bit *= 10;
-	}
-	return (bin);
-}
-/*
- * eight_bits_value
- * 
- * Creates new char 
-*/
-char	*eight_bits_value(void)
-{
-	int		i;
-	char	*newval;
-
-	i = 0;
-	newval = malloc(sizeof(char) * 9);
-	if (!newval)
-		return (0);
-	while (i < 8)
-		newval[i++] = '0';
-	newval[i] = '\0';
-	return (newval);
-}
-/*
- * jsp meme pas frr c bon
- * 
- * 
-*/
-void	send_char(char *message)
+static void	ft_send(int pid, char c)
 {
 	int	i;
-	int	tosend;
 
 	i = 0;
-	while (message[i++])
+	while (i < 8)
 	{
-		tosend = dec_to_bin(message[i]);
+		if (c & 1 << i)
+		{
+			if (pid != -1)
+				kill(pid, SIGUSR1);
+		}
+		else
+		{
+			if (pid != -1)
+				kill(pid, SIGUSR2);
+		}
+		i++;
+		usleep(100);
 	}
-}
-
-int	send_sig_to_pid(int pid, int signum)
-{
-	if (kill(pid, signum) == -1)
-		return (-1);
-	return (0);
 }
 
 int	main(int ac, char **av)
 {
-	int	test;
-	int	signum;
-	int	targetpid;
+	int	i;
+	int	pid;
 
-	if (ac != 3)
-		return (0);
-	printf("coucou je client\n");
-	printf("test fonction eight_bits_value : %s\n", eight_bits_value());
-	signum = SIGUSR1;
-	targetpid = atoi(av[1]);
-	test = dec_to_bin(av[2][0]);
-	printf("parametre 2 : %s\n", av[2]);
-	printf("test en binaire : %d\n", test);
-	if (send_sig_to_pid(targetpid, signum) == 0)
-		printf("Signal envoyé au PID %d avec succès.\n", targetpid);
+	i = 0;
+	if (ac == 3)
+	{
+		pid = atoi(av[1]);
+		if (pid <= 0)
+			return (1);
+		while (av[2][i])
+			ft_send(pid, av[2][i++]);
+		ft_send(pid, '\n');
+	}
 	return (0);
 }
--- minitalk/talkie-server.c
+++ minitalk/talkie-server.c
@@ -12,27 +12,30 @@
 
 #include "minitalk.h"
 
-volatile sig_atomic_t	g_receivedsignal = 0;
-
-void	sig_handler(int signum)
+static void	ft_signal_handler(int sig)
 {
-	(void) signum;
-	g_receivedsignal = 1;
+	static int	i;
+	static char	c;
+
+	if (sig == SIGUSR1)
+		c |= 1 << i;
+	i++;
+	if (i == 8)
+	{
+		write(1, &c, 1);
+		i = 0;
+		c = 0;
+	}
 }
 
 int	main(void)
 {
-	int				handled_sig[] = {SIGINT, SIGTERM, SIGUSR1, SIGUSR2};
-	unsigned long	i;
-
-	i = 0;
-	while (i < sizeof(handled_sig) / sizeof(handled_sig[0]))
-		signal(handled_sig[i++], sig_handler);
-	printf("Le programme s'exécute. Attente d'n signal...\n");
-	printf("Le PID du processus en cours est : %d\n", getpid());
-	while (!g_receivedsignal)
+	write(1, "PID : ", 6);
+	while (1)
 	{
+		signal(SIGUSR1, ft_signal_handler);
+		signal(SIGUSR2, ft_signal_handler);
+		pause();
 	}
-	printf("Signal recu. Le programme se termine. Terminé bonsoir.\n");
 	return (0);
 }
